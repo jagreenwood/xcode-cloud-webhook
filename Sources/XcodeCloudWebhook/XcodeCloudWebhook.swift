241fa6d6ac26500cc58c1c0a44c40f49ed124b9a
Jeremy Greenwood <jagreenwood@gmail.com>
2022-12-19 17:51:26 -0500

    initial implementatino of webhook runner

--- Sources/XcodeCloudWebhook/XcodeCloudWebhook.swift
+++ Sources/XcodeCloudWebhook/XcodeCloudWebhook.swift
@@ -1,6 +1,34 @@
-public struct XcodeCloudWebhook {
-    public private(set) var text = "Hello, World!"
+//
+//  XcodeCloudWebhook.swift
+//
+//
+//  Created by Jeremy Greenwood on 12/12/22.
+//
 
-    public init() {
+public protocol XcodeCloudWebhookHandler {
+    func run(with payload: Payload) async throws
+}
+
+public actor XcodeCloudWebhook {
+    typealias Storage = Array<XcodeCloudWebhookHandler>
+
+    var _storage = Storage()
+
+    public init() {}
+
+    public func add(_ handler: XcodeCloudWebhookHandler) {
+        _storage.append(handler)
+    }
+
+    public func run(_ payload: Payload) async throws {
+        try await withThrowingTaskGroup(of: Void.self) { [unowned self] group in
+            for handler in self._storage {
+                group.addTask {
+                    try await handler.run(with: payload)
+                }
+            }
+
+            try await group.waitForAll()
+        }
     }
 }
